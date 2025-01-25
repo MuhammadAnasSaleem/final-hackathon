ba4d94779a5139720762f2444399d1e83e4540ae
MuhammadAnasSaleem <anassaleem555590@gmail.com>
2025-01-25 19:05:15 +0500

    first

---
+++ database.js
@@ -0,0 +1,34 @@
+const mongodbUri = process.env.MONGO_URI;
+
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(mongodbUri, {
+      dbName: "HACKATHON",
+    });
+
+    console.log(`\n🌿 MongoDB connected ! 🍃\n`);
+
+    mongoose.connection.on(
+      "error",
+      console.error.bind(console, "Connection error:")
+    );
+
+    process.on("SIGINT", () => {
+      // Cleanup code
+      mongoose.connection.close();
+
+      console.log("Mongoose connection closed due to application termination");
+      process.exit(0);
+    });
+  } catch (error) {
+    console.error("MONGODB connection FAILED ", error);
+    process.exit(1); // Exited with error
+  }
+};
+
+try {
+  connectDB();
+} catch (error) {
+  console.log(error);
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return <div className="bg-red-500 text-white text-center ">hello react</div>;
+}
+
+export default App;
