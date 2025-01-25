14da82c87b4b957e87df3dc4fa210478ea48939c
MuhammadAnasSaleem <anassaleem555590@gmail.com>
2025-01-25 19:30:21 +0500

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="bg-red-500 text-white text-center ">hello react</div>;
+  return (
+    <div className="bg-red-500 text-white text-center h-screen w-full ">
+      hello react
+    </div>
+  );
 }
 
 export default App;
